ebccf48e9225a8bcce74ed14748295a08099001f
Dimitri Zhelkov <dimitri.zhelkov@hotmail.com>
2021-02-01 02:12:06 +0100

    Added new pages, added components and added tests

--- Antiproton/PBarNavigation.cs
+++ Antiproton/PBarNavigation.cs
@@ -10,40 +10,47 @@ namespace Antiproton
     public class PBarNavigation : INavigation
     {
         private INavigation _navigation;
+        private PBarDriver _driver;
 
-        public PBarNavigation(INavigation navigation)
+        public PBarNavigation(INavigation navigation, PBarDriver driver)
         {
             _navigation = navigation;
+            _driver = driver;
         }
 
         public void Back()
         {
             Log.GetLogger().Info($"Navigating Back");
             _navigation.Back();
+            _driver.WaitForDocumentState();
         }
 
         public void Forward()
         {
             Log.GetLogger().Info($"Navigating Forward");
             _navigation.Forward();
+            _driver.WaitForDocumentState();
         }
 
         public void GoToUrl(string url)
         {
             Log.GetLogger().Info($"Navigating To Url [{url}]");
             _navigation.GoToUrl(url);
+            _driver.WaitForDocumentState();
         }
 
         public void GoToUrl(Uri url)
         {
             Log.GetLogger().Info($"Navigating To Url [{JsonConvert.SerializeObject(url)}]");
             _navigation.GoToUrl(url);
+            _driver.WaitForDocumentState();
         }
 
         public void Refresh()
         {
             Log.GetLogger().Info($"Refresh page");
             _navigation.Refresh();
+            _driver.WaitForDocumentState();
         }
     }
 }
